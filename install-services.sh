6313e7019acda162c416911b3a2e8f65146c671b
Rodrigo Corrêa <rcelha@gmail.com>
2022-07-03 10:07:09 +0100

    Fix: suspend-then-hibernate deps on services

--- install-services.sh
+++ install-services.sh
@@ -8,6 +8,7 @@ sudo touch /etc/systemd/system/hibernate-preparation.service
 echo '[Unit]
 Description=Enable swap file and disable zram before hibernate
 Before=systemd-hibernate.service
+Before=systemd-suspend-then-hibernate.service
 
 [Service]
 User=root
@@ -15,7 +16,9 @@ Type=oneshot
 ExecStart=/bin/bash -c "/usr/sbin/swapon /swapfile && /usr/sbin/swapoff /dev/zram0"
 
 [Install]
-WantedBy=systemd-hibernate.service' | sudo tee -a /etc/systemd/system/hibernate-preparation.service
+WantedBy=systemd-hibernate.service
+WantedBy=systemd-suspend-then-hibernate.service
+' | sudo tee -a /etc/systemd/system/hibernate-preparation.service
 sudo systemctl enable hibernate-preparation.service
 
 # Disable the swap file when the system just resumed
@@ -24,6 +27,7 @@ sudo touch /etc/systemd/system/hibernate-resume.service
 echo '[Unit]
 Description=Disable swap after resuming from hibernation
 After=hibernate.target
+After=suspend-then-hibernate.target
 
 [Service]
 User=root
@@ -31,7 +35,9 @@ Type=oneshot
 ExecStart=/usr/sbin/swapoff /swapfile
 
 [Install]
-WantedBy=hibernate.target' | sudo tee -a /etc/systemd/system/hibernate-resume.service
+WantedBy=hibernate.target
+WantedBy=suspend-then-hibernate.target
+' | sudo tee -a /etc/systemd/system/hibernate-resume.service
 sudo systemctl enable hibernate-resume.service
 
 # To avoid false positives regarding to swap space, due to the zram device existence, we need to disable some checks
@@ -42,7 +48,8 @@ Environment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1
 EOF
 
 sudo mkdir -p /etc/systemd/system/systemd-hibernate.service.d/
-cat <<-EOF | sudo tee /etc/systemd/system/systemd-hibernate.service.d/override.conf
+sudo mkdir -p /etc/systemd/system/systemd-suspend-then-hibernate.service.d/
+cat <<-EOF | sudo tee /etc/systemd/system/systemd-hibernate.service.d/override.conf /etc/systemd/system/systemd-suspend-then-hibernate.service.d/override.conf
 [Service]
 Environment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1
 EOF
